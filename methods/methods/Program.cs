7e2a1f258cadd5b175ef0e76fe69933385b69436
cvijander <cvija85@gmail.com>
2023-08-19 12:50:53 +0200

    Methods

---
+++ methods/methods/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+   
+    internal class Program
+    {
+        public static int myScore;
+        static void Main(string[] args)
+        {
+            // Create a variable 
+             myScore = 1000;
+            
+            ChangeScore();
+        }
+
+        public static void ChangeScore()
+        {
+            myScore = myScore + 100;
+            Console.WriteLine($"Player score: {myScore}");
+        }
+
+    }
+}
+
+
+
+ 
+
+
+//myScore = myScore + 100;
+//Console.WriteLine($"Player score: {myScore}");
+
+
+
+
+
