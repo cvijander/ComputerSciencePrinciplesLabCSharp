92115c7fc2e921a8a1bd4fc1f627dcd1b165b7ce
cvijander <cvija85@gmail.com>
2023-08-19 20:50:41 +0200

    If else ..else

---
+++ conditionaltest/conditionaltest/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean knowCharp = true;
+            Console.WriteLine($"The value of knowCSharp is {knowCharp}");
+
+        if (knowCharp )
+            {
+                Console.WriteLine("I know C#");
+            }
+        if (knowCharp) Console.WriteLine("I still know C#");
+
+        int myValue = 5;
+        Console.WriteLine("myValue is equal to 5");
+
+        if(myValue == 5)
+        {
+                Console.WriteLine("The condition (myValue == 5 ) is true");
+         }
+
+        if(myValue <5 )
+            {
+                Console.WriteLine("The condition (myValue < 5 ) is true");
+            }
+        else
+            {
+                Console.WriteLine("The condition (myValue <5 ) is false ");
+            }
+
+        if(myValue !=5 )
+            {
+                Console.WriteLine("The condition (myValue != 5) is true");
+            }
+        else if(myValue > 5)
+            {
+                Console.WriteLine("The second condition (myValue > 5) is true");
+            }
+        else
+            {
+                Console.WriteLine("All conditions are false ");
+            }
+
+        if(myValue >= 1)
+            {
+                Console.WriteLine("The condition (myValue >=1) is true");
+                if(myValue <= 5)
+                {
+                    Console.WriteLine("This condition (myValue <=5) is true ");
+
+                }
+            }
+        }
+    }
+}
