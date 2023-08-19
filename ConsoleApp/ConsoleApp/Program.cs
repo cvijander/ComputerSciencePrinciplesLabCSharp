88576ff7e7bc2b6fe995d953e79dfe799ea0b91c
cvijander <cvija85@gmail.com>
2023-08-19 11:25:55 +0200

    first example

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Define a variable with a value 
+
+int myNumber = 5;
+
+// Send the value of the variable to the console 
+Console.WriteLine(myNumber);
