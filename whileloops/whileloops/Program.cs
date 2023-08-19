50139a200a98dbdaa7f28ac3c55c7a69efc39b1a
cvijander <cvija85@gmail.com>
2023-08-19 21:35:31 +0200

    While and do while loops

---
+++ whileloops/whileloops/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myValue = 0;
+            while (myValue < 100 )
+            {
+                if(myValue % 9 == 0)
+                {
+                    Console.WriteLine(myValue);
+                }
+                myValue ++;
+            }
+
+            int myBadValue = 0;
+            do
+            {
+                Console.WriteLine("This will run at least once.");
+            } while (myBadValue != 0 );
+        }
+    }
+}
