35a9838891230d7928dc71dd7e87260109a9647f
cvijander <cvija85@gmail.com>
2023-08-19 22:13:16 +0200

    For loops

---
+++ forloops/forloops/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine($"The square of {i} is {Math.Pow(i, 2)}");
+            }
+
+            /* 1 2  3  4 5 6 7 8 9 10 11 12
+             * 2 4  6  8 10 12 14 16 18 20 22 24
+             * 3.....
+             * 
+             * */
+
+            for (int r =1; r <=12; r++)
+            {
+                string tableRow = "";
+
+                for (int c =1; c <= 12; c++)
+                {
+                    int product = r * c;
+                    string productString = product.ToString() + "\t";
+                    tableRow += productString;
+                }
+
+                Console.WriteLine(tableRow);
+            }
+
+        }
+    }
+}
