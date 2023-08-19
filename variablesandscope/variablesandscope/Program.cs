1f19214b79a5a039fa6df68337e3e5f8a9b9a849
cvijander <cvija85@gmail.com>
2023-08-19 13:38:46 +0200

    scope

---
+++ variablesandscope/variablesandscope/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        // create the score variable for the game 
+        public static int outsideValue = 5;
+
+        // main function of the program 
+        static void Main(string[] args)
+        {
+            // outside variable to the console 
+            Console.WriteLine($"From outside: outsideValue = {outsideValue}");
+            //Console.WriteLine($"From outside: functionValue ={functionValue}");
+
+            MyFunction();
+            MyOtherFunction();
+
+        }
+        public static void MyFunction()
+        {
+            // Create a variable inside of a function
+            // 
+            int functionValue = 10;
+
+            // Output variables to the console 
+            Console.WriteLine($"From outside : outsideValue ={outsideValue}");
+            Console.WriteLine($"From outside : functionValue = {functionValue}");
+        }
+
+        public static void MyOtherFunction()
+        {
+            // output variuables to the console 
+            Console.WriteLine($"From outside: outsideValue = {outsideValue}");
+           // Console.WriteLine($"From outside : functionValue = { functionValue}");
+        }
+    }
+}
