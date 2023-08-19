a917f0016a4fb67b50cc554bd711ff1b489f2645
cvijander <cvija85@gmail.com>
2023-08-19 13:59:57 +0200

    return values

---
+++ returnvalues/returnvalues/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        public static double subtotal;
+
+        //main function of the program 
+        static void Main(string[] args)
+        {
+            subtotal = 15.00;
+
+            Console.WriteLine($"Subtotal: {subtotal}");
+
+            double total = subtotal + CalculateTax(0.08);
+            Console.WriteLine($"Total: {total}");
+        }
+
+        public static double CalculateTax(double taxRate)
+        {
+            double tax = subtotal * taxRate;
+            Console.WriteLine($"Tax:  { tax} ");
+            return tax;
+        }
+    }
+}
