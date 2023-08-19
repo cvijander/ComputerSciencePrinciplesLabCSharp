b1fdb3430615ceed6a8309df75f5cbc7bae3213f
cvijander <cvija85@gmail.com>
2023-08-19 22:20:18 +0200

    Array

---
+++ array/array/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] myArray = new string[] { "Doug", "Mike", "Matt", "Janet" };
+
+Console.WriteLine(myArray[0]);
+
+Console.WriteLine(myArray[4]);
